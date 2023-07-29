1d8d13dbe9d7541a1c2dcb4188aecd8aff441657
Jono <jowonowo@gmail.com>
2023-07-29 16:07:48 -0400

    Minor update

--- wykoj/static/editor/grader.py
+++ wykoj/static/editor/grader.py
@@ -1,7 +1,7 @@
 il = int(input())  # Number of lines in the test case input
-test_input = "".join(input() + "\\n" for _ in range(il))  # Test case input
+test_input = "".join(input() + "\n" for _ in range(il))  # Test case input
 ol = int(input())  # Number of lines in the submission program output
-test_output = "".join(input() + "\\n" for _ in range(ol))  # User program output
+test_output = "".join(input() + "\n" for _ in range(ol))  # User program output
 
 if ...:
     print("AC")  # Accepted
