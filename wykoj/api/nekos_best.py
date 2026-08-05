0d443339050e8e0d164222b82812a0e8d61d76fc
bedrockfake <kodisiu@gmail.com>
2026-08-05 10:47:24 +0800

    fix: user-agent headers when calling NekosBestAPI endpoint

--- wykoj/api/nekos_best.py
+++ wykoj/api/nekos_best.py
@@ -10,8 +10,11 @@ class NekosBestAPI:
     @staticmethod
     async def get_url() -> str:
         try:
+            headers = {
+                "User-Agent": "wykojBot/1.0 (https://oj.wyk.edu.hk/)"
+            }
             url = "https://nekos.best/api/v2/" + random.choice(["waifu", "neko", "kitsune"])
-            response = await current_app.session.get(url)
+            response = await current_app.session.get(url, headers=headers)
         except Exception as e:
             logger.error(
                 f"Error in fetching from nekos.best API:\n{e.__class__.__name__}: {str(e)}"
